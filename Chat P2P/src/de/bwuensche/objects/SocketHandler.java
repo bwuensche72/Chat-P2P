198d183b211c7d5c3689784c89d3cec83fcb0a17
bwuensche72 <benediktwuensche@tutanota.com>
2025-09-03 16:54:25 +0100

    Leerzeilen entfernt

--- Chat P2P/src/de/bwuensche/objects/SocketHandler.java
+++ Chat P2P/src/de/bwuensche/objects/SocketHandler.java
@@ -16,16 +16,17 @@ public class SocketHandler {
     
     /**
      * Dieser Handler kümmert sich um den Ablauf des Chats, welcher gewisse Angaben braucht, um kommunizieren zu können.
-     * Die Setzung des Nicknamens erfolgt nach gewissen Vorgaben: Der Name soll kleiner als 20 Zeichen lang sein. Der
-     * Name soll größer als ein Zeichen lang sein. Der Name soll aus Buchstaben und Zahlen bestehen, nur Leerschritte
-     * sind nicht erlaubt.
+     * Vorgaben für den Nickname: 
+     * - Der Name soll kleiner als 20 Zeichen lang sein.
+     * - Der Name soll mindestens ein Zeichen lang sein. 
+     * - Der Name soll aus Buchstaben und Zahlen bestehen, 
+     *   nur Leerschritte sind nicht erlaubt.
      * 
      * @param host - die gewünschte Adresse
      * @param port - der gewünschte Port
      * @param name - der gewünschte Name des Benutzers
      */
     public SocketHandler(String host, int port) {
-        
         try {
             // Unterscheidung ob ein client oder server erstellt werden soll
             if (host == null) {
@@ -35,17 +36,14 @@ public class SocketHandler {
                     InetAddress adress = socket.getLocalAddress();
                     System.out.println("Lokale Adresse: " + adress + ":" + port);
                 }
-                
             } else {
                 // Client
                 socket = new Socket(host, port);
                 InetAddress adress = socket.getLocalAddress();
                 System.out.println("Verbinde mit dem Server: " + adress + ":" + port);
             }
-            
         } catch (UnknownHostException e) {
             System.out.println("Der Host ist uns unbekannt, versuche es bitte erneut");
-            
         } catch (IOException e) {
             System.out.println("Es ist ein Fehler aufgetreten, bitte erneut versuchen");
         }
@@ -57,7 +55,6 @@ public class SocketHandler {
      * @throws IOException Signalisiert einen Fehler des I/Os in jeglicher Form
      */
     public String readMessage() throws IOException {
-        
         DataInputStream inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
         return inputStream.readUTF();
     }
@@ -69,18 +66,14 @@ public class SocketHandler {
      * @throws IOException Signalisiert einen Fehler des I/Os in jeglicher Form
      */
     public void sendMessage(String message) throws IOException {
-        
         // Auslesung der gesendeten Nachricht
         DataOutputStream outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-        
         // Prüfung ob Inhalt in der Nachricht übermittelt wird
         if (message.isEmpty()) {
             return;
         }
-        
         // senden der Nachricht
         outputStream.writeUTF(message);
         outputStream.flush();
     }
-    
 }
