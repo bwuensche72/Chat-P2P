b7c8eef05d9f08201469da5b9abf54f56c8258d0
bwuensche72 <benediktwuensche@tutanota.com>
2025-09-03 16:58:24 +0100

    Leerzeichen entfernt und Kommentar angepasst

--- Chat P2P/src/de/bwuensche/objects/User.java
+++ Chat P2P/src/de/bwuensche/objects/User.java
@@ -11,27 +11,21 @@ public class User {
     private int port;
     
     /**
-     * Erstellung eines Benutzers des Chats, welcher gewisse Angaben geben soll, um teilnehmen zu können. Die Setzung
-     * des Nicknamens erfolgt nach gewissen Vorgaben: Der Name soll kleiner als 20 Zeichen lang sein. Der Name soll
-     * größer als ein Zeichen lang sein. Der Name soll aus Buchstaben und Zahlen bestehen, nur Leerschritte sind nicht
-     * erlaubt.
+     * Erstellung eines Benutzers des Chats, welcher gewisse Angaben erfordert, um teilnehmen zu können.
+     * Vorgaben für den Nickname: 
+     * - Der Name soll kleiner als 20 Zeichen lang sein.
+     * - Der Name soll mindestens ein Zeichen lang sein. 
+     * - Der Name soll aus Buchstaben und Zahlen bestehen, 
+     *   nur Leerschritte sind nicht erlaubt.
      * 
      * @param host - die gewünschte Adresse
      * @param port - der gewünschte Port
      * @param name - der gewünschte Name des Benutzers
      */
     public User(String host, int port, String nickname) {
-        
-        /* 
-         * Vorgaben für den Nickname: 
-         * - Der Name soll kleiner als 20 Zeichen lang sein. 
-         * - Der Name soll mindestens ein Zeichen lang sein.
-         * - Der Name soll aus Buchstaben und Zahlen bestehen, nur Leerschritte sind nicht erlaubt.
-         */
         if (nickname.length() > 20 || nickname.isEmpty()) {
             throw new IllegalArgumentException("Der Name entspricht nicht den Vorgaben");
         }
-        
         this.host = host;
         this.port = port;
         this.nickname = nickname;
